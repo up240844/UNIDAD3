2cfe5a1e30dbf507c44c5e31d0736355c4442a51
up240844 <up240844@alumnos.upa.edu.mx>
2025-03-03 15:11:56 -0600

    Add files via upload

---
+++ 01_Day_Exercices.py
@@ -0,0 +1,7 @@
+print(4 + 5)
+print(8 - 2)
+print(6 * 2)
+print(8 / 4)
+print(6 ** 3)
+print(5 % 3)
+print(5 // 9)
---
+++ 02_Day_Exercices.py
@@ -0,0 +1,38 @@
+print('Hello, World')
+print(len('hello, World'))
+print(type('Hello, World'))
+print(str("10"))
+print(float(10))
+print(int('10'))
+input('Enter your name:')
+
+#variables in python
+first_name = 'Hector'
+last_name = 'Olivo'
+Contry = 'Mexico'
+city = 'Aguascalientes'
+age = 20
+is_married = False
+skills = ['draw','drive','sing']
+person_info = {
+    'first name':'Hector',
+    'last_name':'Olivo',
+    'contry':'Mexico',
+    'city':'Aguascalientes',
+    }
+
+print('Hello, World')
+print('Hello',',','World','!')
+print(len('Hello, World'))
+
+#4
+print('first_name:',first_name)
+print('first_name length:',len(first_name))
+print('last name:  ', last_name)
+print('Last name length:  ',len(last_name))
+print('country:  ',  country)
+print('City:  ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills:', skills)
+print('Person information:', person_info)
